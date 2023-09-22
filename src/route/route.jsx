13f20d0b79ffeb3457667deaa9cac3829f6c7dea
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-22 19:45:18 +0600

    responsive navbar added

---
+++ src/route/route.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layout/RootLayout/RootLayout";
+import Home from "../components/Home/Home";
+import Statistics from "../components/Statistics/Statistics";
+import AppliedJobs from "../components/AppliedJobs/AppliedJobs";
+import Blog from "../components/Blog/Blog";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout></RootLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/statistics',
+                element: <Statistics></Statistics>
+            },
+            {
+                path: '/applied-jobs',
+                element: <AppliedJobs></AppliedJobs>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            }
+        ]
+    }
+])
+
+export default router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark1': '#1A1919',
+        'dark2': '#474747',
+        'dark3': '#757575',
+        'primary1': '#7E90FE',
+        'primary2': '#9873FF',
+        'light1': '#ffffffb3',
+        'light2': '#ffffff66'
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
