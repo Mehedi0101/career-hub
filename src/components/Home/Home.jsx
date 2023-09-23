edf84fa71e6035cb808c57e602e58913b69db904
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-23 11:06:05 +0600

    featured jobs information added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,21 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
+import { createContext } from "react";
+
+export const JobContext = createContext('');
 
 const Home = () => {
+    const data = useLoaderData();
+
     return (
         <>
             <Banner></Banner>
             <Category></Category>
+            <JobContext.Provider value={data}>
+                <Featured></Featured>
+            </JobContext.Provider>
         </>
     );
 };
