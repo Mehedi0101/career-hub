bdd62f665413a11fe49ba33a8c92fd0e83fd7c5c
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-23 18:16:04 +0600

    spinner added

--- src/layout/RootLayout/RootLayout.jsx
+++ src/layout/RootLayout/RootLayout.jsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import { FadeLoader } from "react-spinners";
+import ScrollToTop from "../../utils/scrollToTop";
 
 const RootLayout = () => {
+
+    const navigation = useNavigation();
+
     return (
         <div>
+            <ScrollToTop />
             <Navbar></Navbar>
             <div className="min-h-[70vh]">
-                <Outlet></Outlet>
+                {
+                    navigation.state === 'loading' ? <div className="flex justify-center items-center h-[70vh]"><FadeLoader color="#9873FF" /></div> : <Outlet></Outlet>
+                }
             </div>
             <div className="bg-dark1">
                 <Footer></Footer>
